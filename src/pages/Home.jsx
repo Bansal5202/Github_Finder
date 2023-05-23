c96e329463955509f6c0298d1a934751b72c4c22
bansal patel <bansalpatel3@gmail.com>
2023-05-23 21:33:59 +0530

    using context

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import UserResult from '../components/users/UserResult'
+function Home() {
+  return (
+    <>
+      <UserResult/>
+    </>
+  )
+}
+
+export default Home
